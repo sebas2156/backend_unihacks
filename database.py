ef939aeeea3e97117b49091c3a869a6a2ddec2cd
Sebastián Antonio Morales Gonzalez <sebastianmoraless257@gmail.com>
2024-10-30 20:08:20 -0400

    credencialesrender

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost:5432/unihacks"
+SQLALCHEMY_DATABASE_URL = "postgresql://baseunihackz_user:1jE4VgmfimhCMNQQeBJfoIhc3ajJNTR5@pg-cshbag56l47c73adrfu0-a.oregon-postgres.render.com:5432/baseunihackz"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
